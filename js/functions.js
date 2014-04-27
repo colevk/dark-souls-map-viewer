4808f2865f59edbe249fabd37d0534af2cc8e82e
Cole van Krieken <colevankrieken@gmail.com>
2014-04-27 13:50:20 -0700

    Remove jspack dependency for faster loading

--- js/functions.js
+++ js/functions.js
@@ -1,29 +1,29 @@
 function loadIVFile(filename, onloadFunction) {
   var request = new XMLHttpRequest();
-  
+
   request.onreadystatechange = function() {
     if (request.readyState == 4) {
       var data = new Uint8Array(request.response);
 
-      var num_models = jspack.unpackFrom('<I', data)[0];
+      var num_models = new Uint32Array(data.buffer.slice(0,4))[0];
 
       for (var i = 0; i < num_models; i++) {
         var model = new THREE.Geometry();
 
-        var modeldata = jspack.unpackFrom('<IIII', data, 16 * (i + 1));
+        var modeldata = new Uint32Array(data.buffer.slice(16 * (i + 1), 16 * (i + 1) + 16));
         var tris_offset  = modeldata[0];
         var num_tris     = modeldata[1] / 3;
         var verts_offset = modeldata[2];
         var num_verts    = modeldata[3];
 
+        var verts = new Float32Array(data.buffer.slice(verts_offset, verts_offset + num_verts * 12));
         for (var j = 0; j < num_verts; j++) {
-          var vert = jspack.unpackFrom('<fff', data, verts_offset + (j * 12));
-          model.vertices.push(new THREE.Vector3(vert[0], vert[1], vert[2]));
+          model.vertices.push(new THREE.Vector3(verts[3 * j], verts[3 * j + 1], verts[3 * j + 2]));
         }
 
+        var tris = new Uint16Array(data.buffer.slice(tris_offset, tris_offset + num_tris * 6));
         for (var j = 0; j < num_tris; j++) {
-          var tri = jspack.unpackFrom('<HHH', data, tris_offset + (j * 6));
-          model.faces.push(new THREE.Face3(tri[0], tri[1], tri[2]));
+          model.faces.push(new THREE.Face3(tris[3 * j], tris[3 * j + 1], tris[3 * j + 2]));
         }
 
         model.computeFaceNormals();
@@ -31,7 +31,7 @@ function loadIVFile(filename, onloadFunction) {
       }
     }
   };
-  
+
   request.open('GET', filename, true);
   request.responseType = 'arraybuffer'
   request.send();
