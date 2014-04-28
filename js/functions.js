61c9c116e6a94860eca7f2d869e2453da1f9c71b
Cole van Krieken <colevankrieken@gmail.com>
2014-04-28 13:50:26 -0700

    Create buffergeometry from scratch

--- js/functions.js
+++ js/functions.js
@@ -8,7 +8,7 @@ function loadIVFile(filename, onloadFunction) {
       var num_models = new Uint32Array(data.buffer.slice(0,4))[0];
 
       for (var i = 0; i < num_models; i++) {
-        var model = new THREE.Geometry();
+        var model = new THREE.BufferGeometry();
 
         var modeldata = new Uint32Array(data.buffer.slice(16 * (i + 1), 16 * (i + 1) + 16));
         var tris_offset  = modeldata[0];
@@ -17,18 +17,53 @@ function loadIVFile(filename, onloadFunction) {
         var num_verts    = modeldata[3];
 
         var verts = new Float32Array(data.buffer.slice(verts_offset, verts_offset + num_verts * 12));
-        for (var j = 0; j < num_verts; j++) {
-          model.vertices.push(new THREE.Vector3(verts[3 * j], verts[3 * j + 1], verts[3 * j + 2]));
-        }
-
         var tris = new Uint16Array(data.buffer.slice(tris_offset, tris_offset + num_tris * 6));
+
+        var normals = new Float32Array(num_tris * 9);
+
+        // calculate normals
         for (var j = 0; j < num_tris; j++) {
-          model.faces.push(new THREE.Face3(tris[3 * j], tris[3 * j + 1], tris[3 * j + 2]));
+          var v1 = tris[3 * j + 0],
+              v2 = tris[3 * j + 1],
+              v3 = tris[3 * j + 2];
+
+          var v1_x = verts[3 * v2 + 0] - verts[3 * v1 + 0],
+              v1_y = verts[3 * v2 + 1] - verts[3 * v1 + 1],
+              v1_z = verts[3 * v2 + 2] - verts[3 * v1 + 2];
+
+          var v2_x = verts[3 * v3 + 0] - verts[3 * v2 + 0],
+              v2_y = verts[3 * v3 + 1] - verts[3 * v2 + 1],
+              v2_z = verts[3 * v3 + 2] - verts[3 * v2 + 2];
+
+          normals[9 * j + 0] = normals[9 * j + 3] = normals[9 * j + 6] = v1_y * v2_z - v1_z * v2_y;
+          normals[9 * j + 1] = normals[9 * j + 4] = normals[9 * j + 7] = v1_z * v2_x - v1_x * v2_z;
+          normals[9 * j + 2] = normals[9 * j + 5] = normals[9 * j + 8] = v1_x * v2_y - v1_y * v2_x;
         }
 
-        model.computeFaceNormals();
-        model.computeVertexNormals();
-        onloadFunction(THREE.BufferGeometryUtils.fromGeometry(model));
+        model.attributes = {
+          position: {
+            itemSize: 3,
+            array: verts
+          },
+          index: {
+            itemSize: 3,
+            array: tris
+          },
+          normal: {
+            itemSize: 3,
+            array: normals
+          }
+        };
+
+        model.normalizeNormals();
+
+        model.offsets = [{
+          start: 0,
+          count: num_tris * 3,
+          index: 0,
+        }];
+
+        onloadFunction(model);
       }
     }
   };
