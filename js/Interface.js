2e286c4b51092ee4ad226cd8e630e85616923ecd
Cole van Krieken <colevankrieken@gmail.com>
2014-05-18 00:51:08 -0700

    Add docs and fix formatting for Interface

--- js/Interface.js
+++ js/Interface.js
@@ -1,7 +1,20 @@
-Interface = function (scene, camera, meshes, material, config) {
+/**
+ * Create functions to bind to the web interface.
+ * @constructor
+ * @param {THREE.Scene} scene The scene to render.
+ * @param {THREE.Camera} camera The camera.
+ * @param {Array.<THREE.Mesh>} meshes All possible meshes to display.
+ * @param {BigShaderMaterial} material The shader material.
+ * @param {Config} config Config data containing filenames and which files
+ *   should be displayed.
+ */
+function Interface(scene, camera, meshes, material, config) {
   var self = this;
 
-  self.setupInterface = function () {
+  /**
+   * Creates input boxes and hidable divs based on the filenames in config.
+   */
+  self.setupInterface = function() {
     $('#ds2').toggle();
 
     for (var i = 0; i < 2; i++) {
@@ -9,7 +22,7 @@ Interface = function (scene, camera, meshes, material, config) {
       var state = [config.ds1State, config.ds2State][i];
       var game = ['ds1', 'ds2'][i];
 
-      for (var j = 0; j < names.length; j++) {
+      for (var j = 0, lenJ = names.length; j < lenJ; j++) {
         var checkbox = $('<input type="checkbox">');
         checkbox.attr('name', game);
         checkbox.attr('value', j);
@@ -17,7 +30,7 @@ Interface = function (scene, camera, meshes, material, config) {
           checkbox.attr('checked', '');
         }
 
-        checkbox.change((function (gameNumber, fileNumber) {
+        checkbox.change((function(gameNumber, fileNumber) {
           return function () {
             self.addRemove(gameNumber, fileNumber, $(this).is(':checked'));
           }
@@ -48,7 +61,10 @@ Interface = function (scene, camera, meshes, material, config) {
     }
   }
 
-  self.toggleBackfaceCulling = function () {
+  /**
+   * Toggle whether or not to show backfaces.
+   */
+  self.toggleBackfaceCulling = function() {
     if (material.side === THREE.DoubleSide) {
       material.side = THREE.FrontSide;
     } else {
@@ -56,43 +72,68 @@ Interface = function (scene, camera, meshes, material, config) {
     }
   }
 
-  self.toggleWrapAround = function () {
+  /**
+   * Toggle whether lighting should drop to 0 at 90 degrees from light or at
+   * 180 degrees from light.
+   */
+  self.toggleWrapAround = function() {
     material.uniforms.wrapAround.value = !material.uniforms.wrapAround.value;
   }
 
-  self.toggleNormalShading = function () {
+  /**
+   * Toggle displaying the surface normal in place of fragment color.
+   */
+  self.toggleNormalShading = function() {
     material.uniforms.normalShading.value = !material.uniforms.normalShading.value;
   }
 
-  self.toggleEdgeHighlighting = function () {
+  /**
+   * Toggle displaying triangle edges.
+   */
+  self.toggleEdgeHighlighting = function() {
     material.uniforms.edgeHighlight.value = !material.uniforms.edgeHighlight.value;
   }
 
-  self.setEdgeColor = function (color) {
+  /**
+   * Set triangle edge color.
+   * @param {string} color A css color string.
+   */
+  self.setEdgeColor = function(color) {
     var tmpColor = new THREE.Color(color);
     material.uniforms.edgeColor.value = new THREE.Vector3(tmpColor.r, tmpColor.g, tmpColor.b);
   }
 
-  self.setLightColor = function (color) {
+  /**
+   * Set light source color.
+   * @param {string} color A css color string.
+   */
+  self.setLightColor = function(color) {
     config.light.color = new THREE.Color(color);
   }
 
-  self.resetCamera = function () {
+  /**
+   * Reset camera to its default position and orientation.
+   */
+  self.resetCamera = function() {
     camera.position = Config.defaultCameraPosition();
     camera.lookAt(Config.defaultCameraLookAt());
   }
 
-  self.swapGames = function (currentGame) {
+  /**
+   * Switch to displaying meshes and interface for a different game.
+   * @param {string} newGame The identifier for the new game.
+   */
+  self.swapGames = function(newGame) {
     $('.file-list').hide();
-    $('#' + currentGame).show();
+    $('#' + newGame).show();
 
     var toAdd = [];
-    $('input[name=' + currentGame + ']:checked').map(function () {
+    $('input[name=' + newGame + ']:checked').map(function() {
       toAdd.push(parseInt($(this).val()));
     });
-    for (var i = 0; i < meshes.length; i++) {
+    for (var i = 0, lenI = meshes.length; i < lenI; i++) {
       var mesh = meshes[i];
-      if (mesh.game === currentGame && toAdd.indexOf(mesh.fileNumber) >= 0) {
+      if (mesh.game === newGame && toAdd.indexOf(mesh.fileNumber) >= 0) {
         scene.add(mesh);
       } else {
         scene.remove(mesh);
@@ -100,8 +141,14 @@ Interface = function (scene, camera, meshes, material, config) {
     }
   }
 
-  self.addRemove = function (game, fileNumber, shouldAdd) {
-    for (var i = 0; i < meshes.length; i++) {
+  /**
+   * Add or remove specified meshes from or to the scene.
+   * @param {string} game The identifier for the game the meshes belong to.
+   * @param {number} fileNumber The file number the meshes are associated with.
+   * @param {boolean} shouldAdd Whether the meshes should be added or removed.
+   */
+  self.addRemove = function(game, fileNumber, shouldAdd) {
+    for (var i = 0, lenI = meshes.length; i < lenI; i++) {
       var mesh = meshes[i];
       if (mesh.game === game && mesh.fileNumber === fileNumber) {
         if (shouldAdd) {
