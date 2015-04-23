d4ddb8918da84645b6e0553d5ccfac5d0f100838
Cole van Krieken <colevankrieken@gmail.com>
2015-04-23 14:44:46 -0700

    Finer-grained file loading.

--- js/Config.js
+++ js/Config.js
@@ -2,45 +2,113 @@
  * Configuration data for setting up the viewer.
  */
 Config = new function () {
-  // Dark Souls 1 filenames.
+  // Dark Souls 1 file info.
   this.ds1 = [
-    {visible: true,  name: "Anor Londo"},
-    {visible: true,  name: "Ash Lake"},
-    {visible: true,  name: "Blighttown + Quelaag's Domain"},
-    {visible: true,  name: "Catacombs"},
-    {visible: true,  name: "Darkroot Garden + Basin"},
-    {visible: true,  name: "Demon Ruins + Lost Izalith"},
-    {visible: true,  name: "Depths"},
-    {visible: true,  name: "Duke's Archive + Crystal Caves"},
-    {visible: true,  name: "Firelink Shrine"},
+    {visible: true, name: "Anor Londo"},
+    {visible: true, name: "Ash Lake"},
+    {visible: true, name: "Blighttown + Quelaag's Domain"},
+    {visible: true, name: "Catacombs"},
+    {visible: true, name: "Darkroot Garden + Basin"},
+    {visible: true, name: "Demon Ruins + Lost Izalith"},
+    {visible: true, name: "Depths"},
+    {visible: true, name: "Duke's Archive + Crystal Caves"},
+    {visible: true, name: "Firelink Shrine"},
     {visible: false, name: "Kiln of the First Flame"},
-    {visible: true,  name: "New Londo Ruins + Valley of Drakes"},
+    {
+      visible: true,
+      name: "New Londo Ruins + Valley of Drakes",
+      using: [{
+        filename: "data/ds1/New Londo Ruins + Valley of Drakes.iv",
+        // cut out the abyss because it interferes with Demon Ruins
+        // (it's just a big circle)
+        chunks: [0, 1, 2, 2, 3],
+        tris: [null, null, [0, 10025], [10067, null], null]
+      }]
+    },
     {visible: false, name: "Oolacile"},
     {visible: false, name: "Painted World of Ariamis"},
-    {visible: true,  name: "Sen's Fortress"},
-    {visible: true,  name: "Tomb of the Giants"},
+    {visible: true, name: "Sen's Fortress"},
+    {visible: false, name: "Tomb of the Giants"},
     {visible: false, name: "Undead Asylum"},
-    {visible: true,  name: "Undead Burg"},
+    {visible: true, name: "Undead Burg"},
   ];
+  _.each(this.ds1, function(info) {
+    if (!("using" in info)) {
+      info.using = [{ filename: "data/ds1/" + info.name + ".iv"}]
+    }
+  })
 
-  // Dark Souls 2 filenames.
+  // Dark Souls 2 file info.
   this.ds2 = [
     {visible: false, name: "Aldia's Keep"},
+    {
+      visible: false,
+      name: "Arenas (Blue Sentinels)",
+      using: [{
+        filename: "data/ds2/Heide's Tower of Flame + Cathedral of Blue.iv",
+        chunks: [5, 6, 7, 8, 9],
+        tris: [null, null, null, null, [0, 17343]]
+      }]
+    },
+    {
+      visible: false,
+      name: "Arenas (Brotherhood of Blood)",
+      using: [{
+        filename: "data/ds2/Huntsman's Copse + Undead Purgatory.iv",
+        chunks: [4, 5, 6],
+        tris: [null, null, [0, 25786]]
+      }]
+    },
     {visible: false, name: "Brightstone Cove Tseldora + Lord's Private Chamber"},
     {visible: false, name: "Dark Chasm of Old"},
     {visible: false, name: "Doors of Pharros"},
-    {visible: false, name: "Dragon Aerie & Dragon Shrine"},
+    {visible: false, name: "Dragon Aerie + Dragon Shrine"},
     {visible: false, name: "Dragon Memories"},
     {visible: false, name: "Drangleic Castle + King's Passage + Throne of Want"},
-    {visible: true,  name: "Forest of Fallen Giants"},
-    {visible: true,  name: "Grave of Saints"},
-    {visible: false, name: "Harvest Valley + Earthen Peak"},
-    {visible: true,  name: "Heide's Tower of Flame + Cathedral of Blue"},
-    {visible: true,  name: "Heide's Tower of Flame"},
-    {visible: false, name: "Huntsman's Copse + Undead Purgatory"},
-    {visible: false, name: "Iron Keep + Belfry Sol"},
-    {visible: true,  name: "Majula (1)"},
-    {visible: true,  name: "Majula (2)"},
+    {visible: true, name: "Forest of Fallen Giants"},
+    {visible: true, name: "Grave of Saints"},
+    {
+      visible: true,
+      name: "Harvest Valley + Earthen Peak",
+      using: [{
+        // Get rid of weird floating area
+        filename: "data/ds2/Harvest Valley + Earthen Peak.iv",
+        chunks: [0, 1, 2, 3, 4],
+        tris: [null, null, null, [974, null], null]
+      }]
+    },
+    {
+      visible: true,
+      name: "Heide's Tower of Flame + Cathedral of Blue",
+      using: [
+        {
+          // Don't include covenant arenas
+          filename: "data/ds2/Heide's Tower of Flame + Cathedral of Blue.iv",
+          chunks: [0, 1, 2, 3, 4, 9, 10],
+          tris: [null, null, null, null, null, [17343, null], null]
+        },
+        {filename: "data/ds2/Heide's Tower of Flame.iv"}
+      ]
+    },
+    {
+      visible: true,
+      name: "Huntsman's Copse + Undead Purgatory",
+      using: [{
+        filename: "data/ds2/Huntsman's Copse + Undead Purgatory.iv",
+        // Don't include covenant arenas
+        chunks: [0, 1, 2, 3, 6, 7],
+        tris: [null, null, null, null, [25786, null], null]
+      }]
+    },
+    {visible: true, name: "Iron Keep + Belfry Sol"},
+    {
+      visible: true,
+      name: "Majula",
+      using: [
+        {filename: "data/ds2/Majula (1).iv"},
+        {filename: "data/ds2/Majula (2).iv"}
+      ]
+    },
     {visible: false, name: "Memory of Vammar, Orro, Jeigh"},
     {visible: true,  name: "No-man's Wharf"},
     {visible: true,  name: "Shaded Woods + Shrine of Winter"},
@@ -50,6 +118,11 @@ Config = new function () {
     {visible: false, name: "Things Betwixt"},
     {visible: false, name: "Undead Crypt"},
   ];
+  _.each(this.ds2, function(info) {
+    if (!("using" in info)) {
+      info.using = [{ filename: "data/ds2/" + info.name + ".iv"}]
+    }
+  })
 
   // Basic lighting.
   this.light = new THREE.DirectionalLight(0xffffff);
--- js/SceneLoader.js
+++ js/SceneLoader.js
@@ -18,7 +18,7 @@ SceneLoader = new function() {
    * @param {function(THREE.BufferGeometry)} onloadFunction Callback to perform
    *   on the loaded BufferGeometries.
    */
-  self.loadIVFile = function(filename, onloadFunction) {
+  self.loadIVFile = function(using, onloadFunction) {
     var request = new XMLHttpRequest();
 
     request.onreadystatechange = function() {
@@ -27,7 +27,28 @@ SceneLoader = new function() {
 
         var num_models = new Uint32Array(data.buffer.slice(0,4))[0];
 
-        for (var i = 0; i < num_models; i++) {
+        // Load only some chunks in file if specified
+        var chunks;
+        if ('chunks' in using) {
+          chunks = using.chunks;
+        } else {
+          chunks = Array.apply(null, {length: num_models}).map(Number.call, Number);
+        }
+
+        // Load only some triangles in chunk if specified
+        var chunk_tri_bounds;
+        if ('tris' in using) {
+          chunk_tri_bounds = _.map(using.tris, function(t) {
+            return (t == null) ? [null, null] : t;
+          });
+        } else {
+          chunk_tri_bounds = _.map(chunks, function(c) { return [null, null]; });
+        }
+
+        _.each(_.zip(chunks, chunk_tri_bounds), function(chunk_info) {
+          var i = chunk_info[0];
+          var tri_range = chunk_info[1];
+
           var model = new THREE.BufferGeometry();
 
           var modeldata = new Uint32Array(data.buffer.slice(16 * (i + 1), 16 * (i + 1) + 16));
@@ -40,20 +61,22 @@ SceneLoader = new function() {
           var verts = new Float32Array(data.buffer.slice(verts_offset, verts_offset + num_verts * 12));
           var tris = new Uint16Array(data.buffer.slice(tris_offset, tris_offset + num_tris * 6));
 
-          var positions = new Float32Array(num_tris * 9);
+          var start = tri_range[0] || 0;
+          var end = tri_range[1] || num_tris;
+          var positions = new Float32Array((end - start) * 9);
 
           // Explode vertices, as we want non-shared attributes for normals
           // and for vertex numbering.
-          for (var j = 0; j < num_tris; j++) {
-            positions[9 * j + 0] = verts[3 * tris[3 * j + 0] + 0];
-            positions[9 * j + 1] = verts[3 * tris[3 * j + 0] + 1];
-            positions[9 * j + 2] = verts[3 * tris[3 * j + 0] + 2];
-            positions[9 * j + 3] = verts[3 * tris[3 * j + 1] + 0];
-            positions[9 * j + 4] = verts[3 * tris[3 * j + 1] + 1];
-            positions[9 * j + 5] = verts[3 * tris[3 * j + 1] + 2];
-            positions[9 * j + 6] = verts[3 * tris[3 * j + 2] + 0];
-            positions[9 * j + 7] = verts[3 * tris[3 * j + 2] + 1];
-            positions[9 * j + 8] = verts[3 * tris[3 * j + 2] + 2];
+          for (var j = start; j < end; j++) {
+            positions[9 * (j - start) + 0] = verts[3 * tris[3 * j + 0] + 0];
+            positions[9 * (j - start) + 1] = verts[3 * tris[3 * j + 0] + 1];
+            positions[9 * (j - start) + 2] = verts[3 * tris[3 * j + 0] + 2];
+            positions[9 * (j - start) + 3] = verts[3 * tris[3 * j + 1] + 0];
+            positions[9 * (j - start) + 4] = verts[3 * tris[3 * j + 1] + 1];
+            positions[9 * (j - start) + 5] = verts[3 * tris[3 * j + 1] + 2];
+            positions[9 * (j - start) + 6] = verts[3 * tris[3 * j + 2] + 0];
+            positions[9 * (j - start) + 7] = verts[3 * tris[3 * j + 2] + 1];
+            positions[9 * (j - start) + 8] = verts[3 * tris[3 * j + 2] + 2];
           }
 
           model.attributes = {
@@ -69,11 +92,11 @@ SceneLoader = new function() {
 
           model.computeVertexNormals();
           onloadFunction(model);
-        }
+        });
       }
     };
 
-    request.open('GET', filename, true);
+    request.open('GET', using.filename, true);
     request.responseType = 'arraybuffer'
     request.send();
   }
